0ba1b0c396ebfa29ecb124eab788142160464aeb
kjw7953 <kjw7953@github.com>
2020-08-01 01:33:35 +0900

    merge maps branch

--- LetsCountPeople/pages/admin.py
+++ LetsCountPeople/pages/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from pages.models import Gym
+from pages.models import Gym, CurrentPeople
 
-admin.site.register(Gym)
+admin.site.register(Gym),
+admin.site.register(CurrentPeople)
