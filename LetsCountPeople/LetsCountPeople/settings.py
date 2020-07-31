8d556756c747161b4e4e278c6c3f363f3f3c311c
cypark <blackco66@gmail.com>
2020-08-01 00:22:06 +0900

    setting.py secret key 지움

--- LetsCountPeople/LetsCountPeople/settings.py
+++ LetsCountPeople/LetsCountPeople/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'lkph-we+2y7y#*63c!z^jom(y-6w#y&#5w67fcr4tkr8q(ya1!'
+SECRET_KEY = os.environ.get('SECRET_KEY', "test_secret_key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = []
 
