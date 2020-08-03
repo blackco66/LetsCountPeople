2ad4eb41a4159f90d6d9a6595abb4d37abf22eca
kjw7953 <kjw7953@github.com>
2020-08-03 17:43:14 +0900

    create review.html 1. 리뷰 작성 2. 리뷰 자세히 보기

--- LetsCountPeople/pages/views.py
+++ LetsCountPeople/pages/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
-from pages.models import Gym
+from pages.models import Gym, CurrentPeople, Review
 
 def index(request):
     gyms = Gym.objects.all()
@@ -8,8 +8,20 @@ def index(request):
 
 
 def review(request):
-  return render(request, 'pages/review.html')
+  reviews = Review.objects.all()
+  return render(request, 'pages/review.html', {'reviews' : reviews})
 
 
 def new(request):
+  if request.method == "POST":
+    gym = Gym.objects.get(id = 1)
+    title = request.POST['review-title']
+    content = request.POST['review-content']
+    Review.objects.create(gym = gym, title = title, content = content)
+    return redirect('/pages/review/')
   return render(request, 'pages/new.html')
+
+
+def show(request, id):
+  review = Review.objects.get(id = id)
+  return render(request, 'pages/show.html', {'review': review})
