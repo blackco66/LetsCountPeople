005020846f589ce82635a3db88d17eb5d5d09907
kjw7953 <kjw7953@github.com>
2020-08-03 16:06:07 +0900

    create review page

--- LetsCountPeople/pages/views.py
+++ LetsCountPeople/pages/views.py
@@ -5,3 +5,11 @@ from pages.models import Gym
 def index(request):
     gyms = Gym.objects.all()
     return render(request, 'pages/index.html', {'gyms': gyms})
+
+
+def review(request):
+  return render(request, 'pages/review.html')
+
+
+def new(request):
+  return render(request, 'pages/new.html')
