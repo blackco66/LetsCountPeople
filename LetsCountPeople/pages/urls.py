776272a19e98179662714f976b7b0dfab135eea6
cypark <blackco66@gmail.com>
2020-08-03 21:28:43 +0900

    added search module

--- LetsCountPeople/pages/urls.py
+++ LetsCountPeople/pages/urls.py
@@ -4,6 +4,7 @@ from django.conf.urls import include
 import pages.views
 
 urlpatterns = [
+    path('', pages.views.search_result, name='search_result'),
     path('', pages.views.index, name='index'),
     path('review/', pages.views.review, name='review'),
 ]
