49097eccc6911365117a8041ea80b0298b292031
kjw7953 <kjw7953@github.com>
2020-08-03 21:50:55 +0900

    리뷰 삭제 기능 구현

--- LetsCountPeople/pages/views.py
+++ LetsCountPeople/pages/views.py
@@ -15,13 +15,32 @@ def review(request):
 def new(request):
   if request.method == "POST":
     gym = Gym.objects.get(id = 1)
+    author = request.user
     title = request.POST['review-title']
     content = request.POST['review-content']
-    Review.objects.create(gym = gym, title = title, content = content)
+    Review.objects.create(gym = gym, author = author, title = title, content = content)
     return redirect('/pages/review/')
   return render(request, 'pages/new.html')
 
 
 def show(request, id):
   review = Review.objects.get(id = id)
+  review.hits += 1
+  review.save()
   return render(request, 'pages/show.html', {'review': review})
+
+
+def update(request, id):
+  review = Review.objects.get(id = id)
+  if request.method == "POST":
+    review.title = request.POST['review-title']
+    review.content = request.POST['review-content']
+    review.save()
+    return render(request, 'pages/show.html', {'review': review})
+  return render(request, 'pages/update.html', {'review': review})
+
+
+def delete(request, id):
+  review = Review.objects.get(id = id)
+  review.delete()
+  return redirect('/pages/review/')
