e780808ea02573171c56f228305beefaf0bfece6
cypark <blackco66@gmail.com>
2020-08-03 21:28:43 +0900

    added search module

--- LetsCountPeople/pages/views.py
+++ LetsCountPeople/pages/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
+from django.db.models import Q
 from pages.models import Gym
+
 
 def index(request):
     gyms = Gym.objects.all()
@@ -8,8 +10,23 @@ def index(request):
 
 
 def review(request):
-  return render(request, 'pages/review.html')
+    return render(request, 'pages/review.html')
 
 
 def new(request):
-  return render(request, 'pages/new.html')
+    return render(request, 'pages/new.html')
+
+
+def search_result(request):
+    gym_names = None
+    query = None
+    gym_all = Gym.objects.all()
+    if 'q' in request.GET:
+        print('search')
+        query = request.GET.get('q')
+        gym_names = gym_all.filter(
+            Q(name__contains=query) | Q(address__contains=query))
+    else:
+        print('nosearch')
+        gym_names = gym_all
+    return render(request, 'pages/index.html', {'query': query, 'gyms': gym_names})
