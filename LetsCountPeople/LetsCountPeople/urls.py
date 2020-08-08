5b5ad058fa82b38267e723bbd021c8f39f9a1eb7
kjw7953 <uis7953@naver.com>
2020-08-08 10:10:02 +0900

    기능 오류 확인 후 수정

--- LetsCountPeople/LetsCountPeople/urls.py
+++ LetsCountPeople/LetsCountPeople/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', pages.views.index, name="index"),
     path('pages/', include('pages.urls')),
-    path('accounts/signup/', accounts.views.signup, name="signup"),
+    # path('accounts/signup/', accounts.views.signup, name="signup"),
     path('accounts/login/', accounts.views.login, name="login"),
     path('accounts/', include('allauth.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
